b9aa8b9676d8f5c82a798a9ff5f0e3e987a0f9c1
Daniel Santillan <ldms99@hotmail.com>
2022-11-18 10:36:12 -0600

    showing stars

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
-
+import styles from "./MovieDetails.module.css";
 
 function MovieDetails() {
 
@@ -23,21 +22,34 @@ function MovieDetails() {
           'Content-Type': 'application/json;charset=utf-8'
         }
       } )
-      setMovie(result);
+      setMovie(result.data);
     }
     fetchMovieDetails();
-  }, [movieId]);
+  }, []);
 
-  console.log(movie.data.title)
+  console.log(movie)
 
-  // const { data: { title } } = movie;
+  const imageUrl = 'https://image.tmdb.org/t/p/w300'
 
+  const stars = Math.round(movie.vote_average / 2)
 
-  // const imageUrl = 'https://image.tmdb.org/t/p/w300'
+  let starsResult = [];
 
+  for (let i = 0; i < stars; i++) {starsResult.push('⭐️')}
 
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.movieDescription}>
+        <img className={styles.poster} src={imageUrl + movie.poster_path} alt={movie.title} />
+        <div className={styles.description}>
+          <h2>{movie.title}</h2>
+          {/* <small>{movie.genres.map((genre) => genre.name).join(', ')}</small> */}
+          <p>{movie.overview}</p>
+          <div className={styles.starsContainer}>
+            {starsResult.map((star) => (<p className={styles.star}>{star}</p>))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
